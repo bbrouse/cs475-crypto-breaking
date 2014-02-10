416d0675ee173898750a5b751819c27496b57028
bbrouse <bbrouse@dramafever.com>
2014-02-10 15:23:05 -0500

    Added functions to xor hex values, and xor two files together

--- helpers.py
+++ helpers.py
@@ -1,6 +1,26 @@
 
 import sys
 import binascii
+from itertools import izip
+
+
+def print_xor_files(filename1, filename2):
+    file1 = open(filename1)
+    file2 = open(filename2)
+    output = ""
+
+    for line1, line2 in izip(file1, file2):
+        xor = xor_hex(line1.strip()[2:], line2.strip()[2:])
+        if len(xor) == 1:
+            xor = '0' + xor
+        output += binascii.unhexlify(xor)
+
+    print output
+
+
+def xor_hex(a, b):
+    result = int(a, 16) ^ int(b, 16)
+    return '{:x}'.format(result)
 
 
 def print_from_file(filename):
@@ -16,4 +36,5 @@ def print_from_file(filename):
     print output
 
 if __name__ == "__main__":
-    print_from_file(sys.argv[1])
+    #print_from_file(sys.argv[1])
+    print_xor_files(sys.argv[1], sys.argv[2])
